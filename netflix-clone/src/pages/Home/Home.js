ac631d3ac0dd9c386acf29296232dd617e291d1c
eyoasAmen <getuyisak@gmail.com>
2024-07-08 05:17:36 +0300

    Header and Footer

---
+++ netflix-clone/src/pages/Home/Home.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Header from "../../componets/Header/Header"
+import Footer from "../../componets/Footer/Footer"
+// import Banner from "../../components/Banner/Banner";
+// import RowList from "../../components/Rows/RowList/RowList";
+
+const Home = () => {
+	return (
+		<>
+			<Header />
+			{/* <Banner />
+			<RowList /> */}
+			<Footer />
+		</>
+	);
+};
+
+export default Home;
